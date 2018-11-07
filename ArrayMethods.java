c994e801079205f6a30ea745a27f9ae840fbbaaf
Kayla Fang <kfang00@stuy.edu>
2018-11-06 21:07:58 -0500

    did rowSum and columnSum

---
+++ ArrayMethods.java
@@ -0,0 +1,23 @@
+public class ArrayMethods {
+	public static int rowSum(int[][] ary, int x){
+		if ((x >= ary.length) || (x < 0)) {
+			return 0;
+		}
+		int sum = 0;
+		for (int i = 0; i < ary[x].length; i++) {
+			sum += ary[x][i];
+		}
+		return sum;
+	}
+ 
+	public static int columnSum(int[][] ary, int x){
+		int sum = 0;
+		for (int i = 0; i < ary.length; i++) {
+			if ((x >= ary[i].length) || (x < 0)) {
+				sum += 0;
+			}
+			else {
+				sum += ary[i][x];
+			}
+		}
+	}
